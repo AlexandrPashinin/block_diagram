083abb524d9d73d87dd7a31b2adbe9a4dee453e5
Alexandr <80054269+AlexandrPashinin@users.noreply.github.com>
2024-01-12 17:34:39 +0300

    dropdown

--- src/components/ControlPanel/index.js
+++ src/components/ControlPanel/index.js
@@ -1,12 +1,14 @@
 // ControlPanel.js
 import React from 'react';
-import { Container, Row, Col, Button, Card, ListGroup, Offcanvas } from 'react-bootstrap';
+import { Container, Row, Col, Button, Card, ListGroup,  } from 'react-bootstrap';
 import { MdAddCircleOutline } from 'react-icons/md';
+import DropdownComponent from '../DropdownPanel';
 
 const ControlPanel = ({ onLayoutChange, handleAddStartNode, handleAddHelloNode }) => (
   <Card className='h-100'>
     <Card.Body>
       <Card.Title>Редактор</Card.Title>
+      <DropdownComponent />
       <Card.Body>
         <ListGroup>
           <ListGroup.Item action onClick={handleAddStartNode}>
@@ -15,7 +17,8 @@ const ControlPanel = ({ onLayoutChange, handleAddStartNode, handleAddHelloNode }
           <ListGroup.Item action onClick={handleAddHelloNode}>
             <MdAddCircleOutline /> Добавить "Привет"
           </ListGroup.Item>
-          {/* Добавьте дополнительные кнопки здесь при необходимости */}
+
+          {/* Добавьте дополнительные кнопки или пункты списка здесь при необходимости */}
         </ListGroup>
       </Card.Body>
     </Card.Body>
